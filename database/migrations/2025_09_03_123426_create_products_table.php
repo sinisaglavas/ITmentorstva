6d6e2745e6637aab9f86bc7ae072c047fac615e0
sinisaglavas <sinisaglavas@ymail.com>
2025-09-03 16:15:27 +0200

    Drugi commit1 - Laravel projekat

--- database/migrations/2025_09_03_123426_create_products_table.php
+++ database/migrations/2025_09_03_123426_create_products_table.php
@@ -14,10 +14,10 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('description')->nullable();
-            $table->string('amount');
-            $table->string('price');
-            $table->string('filename')->nullable();
+            $table->text('description')->nullable();
+            $table->unsignedInteger('amount'); // ne moze biti negativan broj
+            $table->float('price'); // decimal
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
