4c7c00f54e6d6cb91069d3bb1fcd210c3a8b37fc
sinisaglavas <sinisaglavas@ymail.com>
2025-09-30 12:49:12 +0200

    Added prefix to group controller

--- routes/web.php
+++ routes/web.php
@@ -21,21 +21,23 @@ Route::view('/about', 'about');
 Route::view('/contact',  'contact');
 Route::get('/shop', [ShopController::class, 'index']);
 
-Route::middleware(['auth', AdminCheckMiddleware::class])->prefix('admin')->group(function () {
-    Route::controller(ContactController::class)->group(function (){
-        Route::post('/contact/send', 'sendContact')->name('sendContact');
-        Route::get('/contacts', 'getAllContacts')->name('adminAllContacts');
-        Route::put('/contact/update/{contact}', 'update')->name('updateContact');
-        Route::get('/contact/delete/{contact}','delete')->name('deleteContact');
-        Route::get('/contact/update/form/{contact}','updateContactForm')->name('updateContactForm');
-    });
-    Route::controller(ProductController::class)->group(function ()
+Route::middleware(['auth', AdminCheckMiddleware::class])->prefix('/admin')->group(function ()
+{
+    Route::controller(ContactController::class)->prefix('/contact')->group(function ()
     {
-        Route::post('/product/add','addProduct')->name('addProduct');
-        Route::get('/products', 'index')->name('adminAllProducts');
-        Route::get('/products/delete/{product}','delete')->name('deleteProduct');
-        Route::get('/product/update/form/{product}','updateProductForm')->name('updateProductForm');
-        Route::put('/product/update/{product}','update')->name('updateProduct');
+        Route::get('/delete/{contact}','delete')->name('deleteContact');
+        Route::get('/update/form/{contact}','updateContactForm')->name('updateContactForm');
+        Route::get('/all', 'getAllContacts')->name('adminAllContacts');
+        Route::post('/send', 'sendContact')->name('sendContact');
+        Route::put('/update/{contact}', 'update')->name('updateContact');
+    });
+    Route::controller(ProductController::class)->prefix('/product')->group(function ()
+    {
+        Route::get('/all', 'index')->name('adminAllProducts');
+        Route::get('/delete/{product}','delete')->name('deleteProduct');
+        Route::get('/update/form/{product}','updateProductForm')->name('updateProductForm');
+        Route::post('/add','addProduct')->name('addProduct');
+        Route::put('/update/{product}','update')->name('updateProduct');
     });
 });
 
