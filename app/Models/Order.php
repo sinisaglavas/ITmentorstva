d8782551746c9059cefe88786220ad8ffa8df4f7
sinisaglavas <sinisaglavas@ymail.com>
2025-10-11 15:37:11 +0200

    Practice 24.11 - Creating order_items

---
+++ app/Models/Order.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $table = 'orders';
+
+    protected $fillable = [
+        'status',
+        'price',
+        'user_id',
+    ];
+}
---
+++ app/Models/OrderItem.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class OrderItem extends Model
+{
+    protected $table = 'order_items';
+
+    protected $fillable = [
+        'order_id',
+        'product_id',
+        'amount',
+        'price',
+    ];
+}
